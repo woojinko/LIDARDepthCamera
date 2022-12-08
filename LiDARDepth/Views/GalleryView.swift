7e6091df32381ed5c5ad116ffa9d8ae808e2198d
Matt Franchi <mattwfranc@gmail.com>
2022-12-08 12:23:41 -0500

    gallery view start

---
+++ LiDARDepth/Views/GalleryView.swift
@@ -0,0 +1,21 @@
+//
+//  GalleryView.swift
+//  LiDARDepth
+//
+//  Created by Matt Franchi on 12/8/22.
+//  Copyright © 2022 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct GalleryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GalleryView_Previews: PreviewProvider {
+    static var previews: some View {
+        GalleryView()
+    }
+}

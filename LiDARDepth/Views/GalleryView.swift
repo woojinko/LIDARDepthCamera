435b5aea72b5e44d2c3c215218dd21558d1ed27f
Matt Franchi <mattwfranc@gmail.com>
2022-12-08 12:24:34 -0500

    gallery view draft

--- LiDARDepth/Views/GalleryView.swift
+++ LiDARDepth/Views/GalleryView.swift
@@ -8,9 +8,123 @@
 
 import SwiftUI
 
+struct TL_Image: Codable, Identifiable {
+    var id = UUID()
+    let raw: Data
+    let depth: Data
+    
+    //init(raw: UIImage) {
+        //self.raw = raw.pngData()!
+    //}
+    
+}
+
+struct Timelapse: Codable, Identifiable {
+    var id = UUID()
+    let title: String
+    var images: [TL_Image]
+}
+
+class DataProvider: ObservableObject {
+
+    
+    static let shared = DataProvider()
+    private let dataSourceURL: URL
+    @Published var allTimelapses = [Timelapse]()
+    
+    init() {
+        let documentsPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        
+        let timelapsesPath = documentsPath.appendingPathComponent("timelapses").appendingPathExtension("json")
+        dataSourceURL = timelapsesPath
+        
+        _allTimelapses = Published(wrappedValue: getAllTimelapses())
+    }
+    
+    
+    private func getAllTimelapses() -> [Timelapse] {
+        do {
+            let decoder = PropertyListDecoder()
+            let data = try Data(contentsOf: dataSourceURL)
+            var decodedTimelapses = try! decoder.decode([Timelapse].self, from: data)
+            
+            for var decodedTimelapse in decodedTimelapses {
+                decodedTimelapse.images = try! decoder.decode([TL_Image].self, from:data)
+            }
+            
+            return decodedTimelapses
+        } catch {
+            return []
+        }
+        
+    }
+    
+    private func saveTimelapses() {
+        do {
+            let encoder = PropertyListEncoder()
+            let data = try encoder.encode(allTimelapses)
+            try data.write(to: dataSourceURL)
+        } catch {
+            
+        }
+    }
+    
+    func createTimelapse(timelapse: Timelapse) {
+        allTimelapses.insert(timelapse, at: 0)
+        saveTimelapses()
+    }
+    
+    func changeTimelapse(timelapse: Timelapse, index: Int) {
+        allTimelapses[index] = timelapse
+        saveTimelapses()
+    }
+    
+    func deleteTimelapse(_ offsets: IndexSet) {
+        allTimelapses.remove(atOffsets: offsets)
+        saveTimelapses()
+    }
+    
+    func moveTimelapse(source: IndexSet, destination: Int) {
+        allTimelapses.move(fromOffsets: source, toOffset: destination)
+        saveTimelapses()
+    }
+
+}
+
+
+
+
 struct GalleryView: View {
+    
+    let viewColumns = [GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible())]
+    
+    @ObservedObject var dataProvider = DataProvider.shared
+    
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            List {
+                
+                ForEach(dataProvider.allTimelapses) { timelapse in
+                    
+                    LazyVGrid(columns: viewColumns) {
+                        
+                        Text("\(timelapse.title)")
+                            .font(.headline)
+                        
+                        Image(uiImage: (UIImage(data: timelapse.images[0].raw)!))
+                        
+                        
+                        
+                    }
+                    
+                }
+                
+            }
+        }
+        .navigationTitle(Text("Timelapses"))
+        .listStyle(InsetListStyle())
     }
 }
 

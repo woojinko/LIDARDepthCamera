ef862d0e8e89feaae3475ce8ef9b8e012d197de0
Matt Franchi <mattwfranc@gmail.com>
2022-12-09 21:19:51 -0500

    updating app to use new navigationstack class

--- LiDARDepth/Views/ContentView.swift
+++ LiDARDepth/Views/ContentView.swift
@@ -9,6 +9,24 @@ import SwiftUI
 import MetalKit
 import Metal
 
+struct Gallery: Identifiable, Hashable {
+    var id = UUID()
+    let name: String
+}
+
+struct Settings: Identifiable, Hashable {
+    var id = UUID()
+    let name: String
+}
+
+struct Camera: Identifiable, Hashable {
+    var id = UUID()
+    let name: String
+}
+
+
+
+
 struct ContentView: View {
     
     @StateObject private var manager = CameraManager()
@@ -17,75 +35,174 @@ struct ContentView: View {
     @State private var minDepth = Float(0.0)
     @State private var scaleMovement = Float(1.0)
     
+    @State var gallery = Gallery(name: "gallery")
+    
+    @State var settings = Settings(name: "settings")
+    
+    @State var camera = Camera(name: "camera")
+    
     
     let maxRangeDepth = Float(15)
     let minRangeDepth = Float(0)
     
     var body: some View {
-        VStack {
-            HStack {
-                Button {
-                    manager.processingCapturedResult ? manager.resumeStream() : manager.startPhotoCapture()
-                } label: {
-                    Image(systemName: manager.processingCapturedResult ? "play.circle" : "camera.circle")
-                        .font(.largeTitle)
-                }
-            }
-            ScrollView {
-                if manager.dataAvailable {
-                    MetalTextureColorThresholdDepthView(
-                        rotationAngle: rotationAngle,
-                        maxDepth: $maxDepth,
-                        minDepth: $minDepth,
-                        capturedData: manager.capturedData
-                    )
-                    .aspectRatio(calcAspect(orientation: viewOrientation, texture: manager.capturedData.depth), contentMode: .fit)
-                    NavigationView {
-                        VStack {
-                            NavigationLink(destination: {
-                                GalleryView()
-                            }){
-                                VStack{
-                                    Image(systemName: "eyes").resizable().aspectRatio(contentMode: .fit).frame(maxWidth:.infinity, maxHeight: 200)
-                                    Text("Gallery View")
-                                }.frame(maxHeight:200)
-                            } .aspectRatio(calcAspect(orientation: viewOrientation, texture: manager.capturedData.depth), contentMode: .fit)
-                        }
-                    }
-                    NavigationView {
-                        VStack {
-                            NavigationLink(destination: {
-                                MetalPointCloudView(
-                                    rotationAngle: rotationAngle,
-                                    maxDepth: $maxDepth,
-                                    minDepth: $minDepth,
-                                    scaleMovement: $scaleMovement,
-                                    capturedData: manager.capturedData
-                                )
-                            }){
-                                VStack{
-                                    Image(systemName: "allergens").resizable().aspectRatio(contentMode: .fit).frame(maxWidth:.infinity, maxHeight: 200)
-                                    Text("Point Cloud")
-                                }.frame(maxHeight:200)
+        
+        GeometryReader { geometry in
+            
+            
+            VStack {
+                
+                
+                
+                NavigationStack {
+                    
+                    ZStack {
+                        
+                        // Camera view
+                        MetalTextureColorThresholdDepthView(
+                            rotationAngle: rotationAngle,
+                            maxDepth: $maxDepth,
+                            minDepth: $minDepth,
+                            capturedData: manager.capturedData
+                        )
+                        
+                        // Camera button
+                        HStack {
+                            Button {
+                                manager.processingCapturedResult ? manager.resumeStream() : manager.startPhotoCapture()
+                            } label: {
+                                Image(systemName: manager.processingCapturedResult ? "play.circle" : "camera.circle")
+                                    .font(.largeTitle)
                             }
-                            .aspectRatio(calcAspect(orientation: viewOrientation, texture: manager.capturedData.depth), contentMode: .fit)
                         }
+                        .position(x: geometry.size.width / 2, y: geometry.size.height * 0.05)
+                        
+                        
+                        
+                        HStack {
+                            
+                            
+                            
+                            NavigationLink(value: gallery) {
+                                HStack {
+                                    Text("Gallery")
+                                        .font(.title3)
+                                }
+                            }
+                            
+                            Spacer()
+                            
+                            NavigationLink(value: settings) {
+                                HStack {
+                                    Text("Settings")
+                                        .font(.title3)
+                                }
+                            }
+                            
+                        }
+                        
+                        .frame(width: geometry.size.width * 0.4, height: geometry.size.height * 0.03, alignment: .center)
+                        .padding()
+                        .foregroundColor(.black.opacity(0.8))
+                        .background(Color.white.opacity(0.5))
+                        .position(x: geometry.size.width * 0.5, y: geometry.size.height * 0.95)
+                        
+                        .navigationDestination(for: Camera.self) { camera in
+                            
+                        }
+                        
+                        .navigationDestination(for: Gallery.self) { gallery in
+                            GalleryView()
+                        }
+                        
+                        .navigationDestination(for: Settings.self) { settings in
+                            Text("Settings View")
+                        }
+                        
+                        
                     }
                     
-                    NavigationView {
-                        VStack {
-                            NavigationLink(destination: {
-                                MyPointCloudView(capturedData: manager.capturedData)
-                            }){
-                                VStack{
-                                    Image(systemName: "cloud").resizable().aspectRatio(contentMode: .fit).frame(maxWidth:.infinity, maxHeight: 200)
-                                    Text("My Point Cloud")
-                                }.frame(maxHeight:200)
-                            }
-                            .aspectRatio(calcAspect(orientation: viewOrientation, texture: manager.capturedData.depth), contentMode: .fit)
-                        }
-                    }
+                    
+                    
+                    
+                    
                 }
+                
+                
+                /*ScrollView {
+                 if manager.dataAvailable {
+                 
+                 
+                 
+                 
+                 NavigationView {
+                 
+                 VStack {
+                 NavigationLink(destination: {
+                 
+                 
+                 }) {
+                 
+                 VStack{
+                 Image(systemName: "eyes").resizable().aspectRatio(contentMode: .fit).frame(maxWidth: 200, maxHeight: 200)
+                 Text("Camera View")
+                 }.frame(maxHeight:200)
+                 }
+                 
+                 }
+                 
+                 
+                 VStack {
+                 NavigationLink(destination: {
+                 GalleryView()
+                 }){
+                 VStack{
+                 Image(systemName: "eyes").resizable().aspectRatio(contentMode: .fit).frame(maxWidth: 200, maxHeight: 200)
+                 Text("Gallery View")
+                 }.frame(maxHeight:200)
+                 }
+                 }
+                 
+                 VStack {
+                 NavigationLink(destination: {
+                 MetalPointCloudView(
+                 rotationAngle: rotationAngle,
+                 maxDepth: $maxDepth,
+                 minDepth: $minDepth,
+                 scaleMovement: $scaleMovement,
+                 capturedData: manager.capturedData
+                 )
+                 }){
+                 VStack{
+                 Image(systemName: "allergens").resizable().aspectRatio(contentMode: .fit).frame(maxWidth:200, maxHeight: 200)
+                 Text("Point Cloud")
+                 }.frame(maxHeight:200)
+                 }
+                 
+                 }
+                 
+                 VStack {
+                 NavigationLink(destination: {
+                 MyPointCloudView(capturedData: manager.capturedData)
+                 }){
+                 VStack{
+                 Image(systemName: "cloud").resizable().aspectRatio(contentMode: .fit).frame(maxWidth:200, maxHeight: 200)
+                 Text("My Point Cloud")
+                 }.frame(maxHeight:200)
+                 }
+                 
+                 }
+                 
+                 
+                 
+                 
+                 
+                 }
+                 
+                 
+                 
+                 }
+                 }*/
             }
         }
     }

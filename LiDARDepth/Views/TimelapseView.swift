d68acbfb3137eec0d2ffe9844a90f3704743825e
Matt Franchi <mattwfranc@gmail.com>
2022-12-11 14:40:53 -0500

    added timelapse view

---
+++ LiDARDepth/Views/TimelapseView.swift
@@ -0,0 +1,21 @@
+//
+//  TimelapseView.swift
+//  LiDARDepth
+//
+//  Created by Matt Franchi on 12/11/22.
+//  Copyright © 2022 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct TimelapseView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TimelapseView_Previews: PreviewProvider {
+    static var previews: some View {
+        TimelapseView()
+    }
+}

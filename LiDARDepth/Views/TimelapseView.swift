683f43bb90225fa841303aa6a92b618e308aac1f
Matt Franchi <mattwfranc@gmail.com>
2022-12-11 18:02:07 -0500

    timelapse view fading functionality

--- LiDARDepth/Views/TimelapseView.swift
+++ LiDARDepth/Views/TimelapseView.swift
@@ -12,21 +12,46 @@ struct TimelapseView: View {
     
     var timelapse: Timelapse
     
+    @State private var timeline = 0.0
+    @State private var isEditing: Bool = false
+    
     var body: some View {
         
         GeometryReader { geometry in
             
-            Text(timelapse.title)
-            ScrollView(.horizontal) {
-                HStack {
-                    ForEach(timelapse.images, id: \.self) { tl_image in
+            
+            VStack {
+                
+                Text(timelapse.title)
+                
+                ZStack {
+                    ForEach(Array(timelapse.images.enumerated()), id: \.element) { index, tl_image in
                         
                         Image(uiImage: UIImage(data: tl_image.raw)!)
                             .resizable()
                             .frame(width: geometry.size.width * 0.5, height: geometry.size.height * 0.5, alignment: .center)
-                        
+                            //.opacity(Double(1 - (timeline - (index+1) * 10)/10))
+                            .zIndex(Double(timelapse.images.count - index))
+                            .opacity(1.0 - (Double(timeline - (Double(index) * 10.0))/10.0))
+                               
                     }
                 }
+                .onChange(of: timeline) { newTimeline in
+                    print((1.0 - (Double(timeline - (Double(0) * 10.0))/10.0)))
+                }
+                
+                Slider(
+                    value: $timeline,
+                    in: 0...(Double(timelapse.images.count) * 10.0),
+                    step: 1
+                ) {
+                    Text("Timeline")
+                } onEditingChanged: { editing in
+                    isEditing = editing
+                    
+                }
+                .frame(width: geometry.size.width * 0.9, height: geometry.size.height * 0.1, alignment: .center)
+                
             }
         }
         
